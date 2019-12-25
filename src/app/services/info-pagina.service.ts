c66ba0ec37649c7919bba7d3912d6c7f70b83925
ManuelCastillo <Castlem791@gmail.com>
2019-12-25 10:10:08 -0600

    Avance Busqueda

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -8,15 +8,30 @@ import { InfoPagina } from '../interfaces/info-pagina.service';
 export class InfoPaginaService {
   info:InfoPagina={};
   cargada= false;
-
+  equipo: any[]=[];
   constructor(private http:HttpClient) {
     //console.log("Hola");
-this.http.get('assets/data/data-pagina.json')
-.subscribe((resp:InfoPagina)=>{
-  this.cargada=true;
-  this.info= resp;
-  console.log(resp);
-});
+this.cargarInfo();
+this.cargarEquipo();
+   }
 
+   private cargarInfo(){
+    this.http.get('assets/data/data-pagina.json')
+    .subscribe((resp:InfoPagina)=>{
+      this.cargada=true;
+      this.info= resp;
+    
+    
+    });
+   }
+   private cargarEquipo(){
+    this.http.get('https://angular-561df.firebaseio.com/equipo.json')
+    .subscribe( (resp: any[]) =>{
+  
+      this.equipo = resp;
+      console.log(resp);
+    
+    
+    });
    }
 }
